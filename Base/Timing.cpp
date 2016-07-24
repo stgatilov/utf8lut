2ea4b21a031877796b092ca1f84697c19e093636
stgatilov <stgatilov@gmail.com>
2016-07-24 19:44:08 +0700

    Moved PRintTimings function to cpp.

--- Base/Timing.cpp
+++ Base/Timing.cpp
@@ -1,3 +1,13 @@
 #include "Base/Timing.h"
 
 TimingData timingData = TimingData();
+
+void TimingPrintAll() {
+	#define TIMING_X_PRINT(name, idx) \
+		printf("slot %d %10s : %6.3lf cyc/el  %12" PRIu64 " elems\n", \
+			idx, #name, \
+			timingData.totalTime[idx] / (timingData.totalElems[idx] + 1e-9), \
+			timingData.totalElems[idx] \
+		);
+	TIMING_SLOTS(TIMING_X_PRINT);
+}
