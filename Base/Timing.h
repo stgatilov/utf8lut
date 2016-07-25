a531c85b8eb22b32d193195d34c011c650f99efc
stgatilov <stgatilov@gmail.com>
2016-07-25 13:16:45 +0700

    Fixed out of bounds error when printing timings.

--- Base/Timing.h
+++ Base/Timing.h
@@ -56,6 +56,7 @@ extern TimingData timingData;
 
 inline void TimingPrintAll() {
 	#define TIMING_X_PRINT(name, idx) \
+		if (idx == TIMING_MAX) return; \
 		printf("slot %d %10s : %6.3f cyc/el  %12" PRIu64 " elems\n", \
 			idx, #name, \
 			timingData.totalTime[idx] / (timingData.totalElems[idx] + 1e-9), \
