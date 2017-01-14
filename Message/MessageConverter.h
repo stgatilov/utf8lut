1da47ea37540cf3ff4f70f799c68e2e65a4d5a85
Stepan Gatilov <stgatilov@gmail.com>
2017-01-14 20:06:55 +0700

    Added old WIP code.

---
+++ Message/MessageConverter.h
@@ -0,0 +1,81 @@
+#pragma once
+
+#include "Buffer/BaseBufferProcessor.h"
+#include "Buffer/ProcessorPlugins.h"
+
+enum ConversionStatus {
+	csSuccess = 0,
+	//Conversion performed successfully.
+	//Input data is correct and complete.
+	//Output was properly written.
+
+	csIncompleteData = 1,
+	//Input data is correct but INCOMPLETE.
+	//Converted and written to output maximal number of valid characters from input.
+
+	csOverflowPossible = 2,
+	//Conversion not attempted.
+	//Output buffer is not large enough to hold any potential converted output.
+
+	csIncorrectData = 3,
+	//Input data is INCORRECT.
+	//Conversion is done partially, but cannot be finished due to invalid input.
+	//Output buffer contains converted data for some part of the input (generally can be ignored).
+};
+
+struct ConversionResult {
+	//overall result of conversion (see above)
+	ConversionStatus status;
+	//number of bytes written to output
+	long long outputSize;
+};
+
+
+//======================================= In-memory conversion ====================================
+
+struct ConvertInMemorySettings {
+	//if true, then output is written using non-temporal writes
+	//bool writeNonTemporal;
+
+	ConvertInMemorySettings() /*: writeNonTemporal(true)*/ {}
+};
+
+//Convert from contiguous buffer in memory into another (non-overlapping) contiguous buffer in memory.
+ConversionResult ConvertInMemory(BaseBufferProcessor &processor, const char *inputBuffer, long long inputSize, char *outputBuffer, long long outputSize, ConvertInMemorySettings settings = ConvertInMemorySettings());
+
+//Request information about size of output buffer in ConvertInMemory routine.
+//Returns minimal allowed size of output buffer in bytes.
+//If outputBuffer points to char* with null value, then:
+//   1. a buffer of that size will be allocated (with new char[]);
+//   2. pointer to this new buffer will be saved into the pointer;
+long long ConvertInMemorySize(BaseBufferProcessor &processor, long long inputSize, char **outputBuffer = 0);
+
+
+//======================================= On-disk conversion ======================================
+
+enum FileIOType {
+	ftLibC = 0,
+	//Use fopen + fread + fwrite + fclose.
+	//Cross-platform.
+
+	/*ftPosix = 1,
+	//open + read + write + close
+	//Note: requires Posix OS  (Windows also implements this API).*/
+
+	//Perhaps we can add some more types...
+	//ftWinApi,
+	//ftPosixAsync,
+	//ftWinApiAsync,
+	//...
+};
+
+struct ConvertFilesSettings {
+	//which functions to use for IO
+	FileIOType type;
+	/*//hint about buffer size used for I/O requests
+	int bufferSize;*/
+
+	ConvertFilesSettings() : type(ftLibC), bufferSize(0) {}
+};
+
+ConversionResult ConvertFiles(BaseBufferProcessor &processor, const char *inputFilePath, const char *outputFilePath, ConvertFilesSettings settings = ConvertFilesSettings());
---
+++ Tests.cpp
@@ -0,0 +1,307 @@
+#include <vector>
+#include <random>
+#include <stdint.h>
+
+#define RND std::mt19937
+
+typedef std::vector<uint8_t> Data;
+enum Format { Utf8, Utf16, Utf32 };
+
+//simple string operations
+Data operator+ (const Data &a, const Data &b) {
+	Data res = a;
+	res.insert(res.end(), b.begin(), b.end());
+	return res;
+}
+Data Substr(const Data &data, int start, int end = -1) {
+	if (end < 0) end = data.size();
+	start = std::max(std::min(start, data.size()), 0);
+	end = std::max(std::min(end, data.size()), start);
+	return Data(data.begin() + start, data.begin() + end);
+}
+Data Reverse(const Data &data) {
+	Data res = data;
+	std::reverse(res.begin(), res.end());
+	return res;
+}
+
+class BaseGenerator {
+	Format format;	
+	RND &rnd;
+
+	typedef std::uniform_int_distribution<int> Distrib;
+
+public:
+	static const int MaxCode = 0x10FFFF;
+	static const int MaxBytes = 4;
+	static int MaxCodeOfSize(int bytes) {
+		if (bytes == 0)	return -1;
+		if (bytes == 1) return 0x0000007F;
+		if (bytes == 2) return 0x000007FF;
+		if (bytes == 3) return 0x0000FFFF;
+		if (bytes == 4) return MaxCode;
+		assert(0);
+	}
+
+	DataGenerator(Format format, RND &rnd) : format(format), rnd(rnd) {}
+
+//=============== Data generation ==================
+
+	static void WriteWord(Data &data, uint8_t word) {
+		data.push_back(code & 0xFFU);
+	}
+	static void WriteWord(Data &data, uint16_t word) {
+		data.push_back(code & 0xFFU); word >>= 8;
+		data.push_back(code & 0xFFU);
+	}
+	static void WriteWord(Data &data, uint32_t word) {
+		data.push_back(code & 0xFFU); word >>= 8;
+		data.push_back(code & 0xFFU); word >>= 8;
+		data.push_back(code & 0xFFU); word >>= 8;
+		data.push_back(code & 0xFFU);
+	}
+
+	void AddChar(Data &data, int code) const {
+		assert(code >= 0 && code <= MaxCode);
+		if (format == Utf32) {
+			WriteWord(uint32_t(code));
+		}
+		else if (format == Utf16) {
+			//from https://ru.wikipedia.org/wiki/UTF-16#.D0.9A.D0.BE.D0.B4.D0.B8.D1.80.D0.BE.D0.B2.D0.B0.D0.BD.D0.B8.D0.B5
+			if (code < 0x10000)
+				WriteWord(uint16_t(code));
+			else {
+				code = code - 0x10000;
+				int lo10 = (code & 0x03FF);
+				int hi10 = (code >> 10);
+				WriteWord(uint16_t(0xD800 | hi10));
+				WriteWord(uint16_t(0xDC00 | lo10));
+			}
+		}
+		else if (format == Utf8) {
+			//from http://stackoverflow.com/a/6240184/556899
+			if (code <= 0x0000007F)				//0xxxxxxx
+				WriteWord(uint8_t(0x00 | ((code >>  0) & 0x7F)));
+			else if (code <= 0x000007FF) {		//110xxxxx 10xxxxxx
+				WriteWord(uint8_t(0xC0 | ((code >>  6) & 0x1F)));
+				WriteWord(uint8_t(0x80 | ((code >>  0) & 0x3F)));
+			}
+			else if (code <= 0x0000FFFF) {		//1110xxxx 10xxxxxx 10xxxxxx
+				WriteWord(uint8_t(0xE0 | ((code >> 12) & 0x0F)));
+				WriteWord(uint8_t(0x80 | ((code >>  6) & 0x3F)));
+				WriteWord(uint8_t(0x80 | ((code >>  0) & 0x3F)));
+			}
+			else {								//11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+				WriteWord(uint8_t(0xF0 | ((code >> 18) & 0x07)));
+				WriteWord(uint8_t(0x80 | ((code >> 12) & 0x3F)));
+				WriteWord(uint8_t(0x80 | ((code >>  6) & 0x3F)));
+				WriteWord(uint8_t(0x80 | ((code >>  0) & 0x3F)));
+			}
+		}
+		else assert(0);
+	}
+
+	uint8_t RandomByte() {
+		uint8_t byte = Distrib(minCode, maxCode)(rnd);
+		return byte;
+	}
+	int RandomCode(int mask = 0) {
+		if (mask == 0) mask = (1 << MaxBytes) - 1;
+		assert(mask > 0 && mask < );
+
+		int bytes;
+		do {
+			bytes = Distrib(1, MaxBytes)(rnd);
+		} while (!(mask & (1<<bytes)));
+
+		int minCode = MaxCodeOfSize(bytes - 1) + 1;
+		int maxCode = MaxCodeOfSize(bytes);
+		int code = Distrib(minCode, maxCode)(rnd);
+
+		return code;
+	}
+	std::vector<uint8_t> RandomBytes(int size) {
+		std:vector<uint8_t> res;
+		for (int i = 0; i < size; i++)
+			res += RandomByte();
+		return res;
+	}
+	std::vector<int> RandomCodes(int size, int mask = 0) {
+		std:vector<int> res;
+		for (int i = 0; i < size; i++)
+			res += RandomCode(mask);
+		return res;
+	}
+
+	Data ByteToData(uint8_t byte) const {
+		return Data(1, byte);
+	}
+	Data CodeToData(int code) const {
+		Data res;
+		AddChar(res, code);
+		return res;
+	}
+	template<typename BytesContainer>
+	Data BytesToData(BytesContainer bytes) {
+		Data res;
+		for (auto it = std::begin(bytes); it != std::end(bytes); it++) {
+			uint8_t byte = *it;
+			res.push_back(byte);
+		}
+		return res;
+	}
+	template<typename CodesContainer>
+	Data CodesToData(CodesContainer codes) {
+		Data res;
+		for (auto it = std::begin(codes); it != std::end(codes); it++) {
+			int code = *it;
+			AddChar(res, code);
+		}
+		return res;
+	}
+
+//=============== Data splits ==================
+
+	//always finds a split at range [pos .. pos+3]
+	//if input is correct, then split is surely between two chars
+	//if input is invalid, then any split can be returned
+	int FindSplit(const Data &data, int pos) const {
+		pos = std::max(std::min(pos, data.size()), 0);
+		if (format == Utf32) {
+			//align with 32-bit words
+			while (pos & 3) pos++;
+		}
+		else if (format == Utf16) {
+			//align with 16-bit words
+			while (pos & 1) pos++;
+			//check if in the middle of surrogate pair
+			if (pos + 1 < data.size()) {
+				uint16_t val = data[pos] + (data[pos+1] << 8);
+				if (val >= 0xDC00U && val < 0xE000U)
+					pos += 2;	//move past surrogate's second half
+			}
+		}
+		else if (format == Utf8) {
+			//move past continuation bytes (at most 3 of them)
+			for (int i = 0; i < 3; i++)
+				if (pos < data.size() && data[pos] >= 0x80U && data[pos] < 0xC0U)
+					pos++;
+		}
+		pos = std::min(pos, int(data.size()));
+		return pos;
+	}
+
+	int RandomPos(const Data &data) {
+		int pos = Distrib(0, int(data.size()));
+		return pos;
+	}
+	int RandomSplit(const Data &data) {
+		int pos = RandomPos(data);
+		pos = FindSplit(data, pos);
+		return pos;
+	}
+};
+
+
+class TestsGenerator : public BaseGenerator {
+	int mutRad;
+
+public:
+	TestsGenerator (Format format, RND &rnd) : BaseGenerator(format, rnd) {
+		SetMutationRadius();
+	}
+
+	//how close mutations are to each other (if there are several of them)
+	void SetMutationRadius(int rad = 10) {
+		mutRad = rad;
+	}
+
+	int PosByHint(int hint = -1) {
+		if (hint < 0)
+			hint = RandomPos(data);
+		hint += Distrib(-mutRad, mutRad)(rnd);
+		return hint;
+	}
+
+//============ Mutations of single data ===============
+
+	int MutateDoubleBytes(Data &data, int hint = -1) {
+		int pos = PosByHint(hint);
+		int next = pos + Distrib(1, mutRad)(rnd);
+		data = Substr(data, 0, pos) + Substr(data, pos, next) + Substr(data, pos, next) + Substr(data, next);
+		return next;
+	}
+	int MutateDoubleChars(Data &data, int hint = -1) {
+		int pos = FindSplit(data, PosByHint(hint));
+		int next = FindSplit(data, pos + Distrib(1, mutRad)(rnd));
+		data = Substr(data, 0, pos) + Substr(data, pos, next) + Substr(data, pos, next) + Substr(data, next);
+		return next;
+	}
+
+	int MutateAddRandomBytes(Data &data, int hint = -1) {
+		int pos = PosByHint(hint);
+		data = Substr(data, 0, pos) + BytesToData(RandomBytes(Distrib(1, mutRad)(rnd))) + Substr(data, pos);
+		return pos;
+	}
+	int MutateAddRandomChar(Data &data, int hint = -1) {
+		int pos = FindSplit(data, PosByHint(hint));
+		data = Substr(data, 0, pos) + CodesToData(RandomCodes(Distrib(1, mutRad)(rnd))) + Substr(data, pos);
+		return pos;
+	}
+
+	int MutateRemoveRandomBytes(Data &data, int hint = -1) {
+		int pos = PosByHint(hint);
+		int next = pos + Distrib(1, mutRad)(rnd);
+		data = Substr(data, 0, pos) + Substr(data, next);
+		return pos;
+	}
+	int MutateRemoveRandomChars(Data &data, int hint = -1) {
+		int pos = FindSplit(data, PosByHint(hint));
+		int next = FindSplit(data, pos + Distrib(1, mutRad)(rnd));
+		data = Substr(data, 0, pos) + Substr(data, next);
+		return pos;
+	}
+
+	int MutationRevertBytes(Data &data, int hint = -1) {
+		int pos = PosByHint(hint);
+		int next = pos + Distrib(1, mutRad)(rnd);
+		data = Substr(data, 0, pos) + Reverse(Substr(data, pos, next)) + Substr(data, next);
+		return pos;
+	}
+
+	int MutationShortenEnd(Data &data, int hint = -1) {
+		int num = Distrib(1, mutRad)(rnd)
+		if (Distrib(0, 1)(rnd))
+			data = Substr(data, 0, data.size() - num);
+		else 
+			data = Substr(data, num);
+		return hint;
+	}
+
+//============ Mixes of several data ================
+
+	static Data MixConcatenate(const Data &a, const Data &b) {
+		return a + b;
+	}
+	Data MixOneInsideOther(const Data &a, const Data &b) {
+		int pos = RandomPos(a);
+		return Substr(a, 0, pos) + b + Substr(a, pos);
+	}
+	Data MixInterleaveBytes(const Data &a, const Data &b) {
+		Data res;
+		int pa = 0, pb = 0;
+		while (pa < a.size() || pb < b.size()) {
+			int q = Distrib(0, 1)(rnd);
+			if (q == 0 && pa < a.size())
+				res.push_back(a[pa++]);
+			if (q == 1 && pb < b.size())
+				res.push_back(b[pb++]);
+		}
+		return res;
+	}
+
+};
+
+void RunTest(const Data &data) {
+	
+}
