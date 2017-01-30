03a52a359bdc5c1297963fa54df08afda74af1fa
Stepan Gatilov <stgatilov@gmail.com>
2017-01-31 00:26:52 +0700

    Tweaked comment style.

--- src/message/MessageConverter.h
+++ src/message/MessageConverter.h
@@ -6,27 +6,27 @@
 
 enum ConversionStatus {
     csSuccess = 0,
-    //Conversion performed successfully.
-    //The input data is correct and complete.
-    //Output was properly written.
+    // Conversion performed successfully.
+    // The input data is correct and complete.
+    // Output was properly written.
 
     csOverflowPossible = 1,
-    //Conversion not attempted.
-    //Output buffer is not large enough to hold any potential converted output.
+    // Conversion not attempted.
+    // Output buffer is not large enough to hold any potential converted output.
 
     csIncompleteData = 2,
-    //Input data is correct but INCOMPLETE.
-    //All complete code points were converted and written to the output.
+    // Input data is correct but INCOMPLETE.
+    // All complete code points were converted and written to the output.
 
     csIncorrectData = 3,
-    //Input data is INCORRECT.
-    //Conversion is done partially, but cannot be finished due to invalid input.
-    //Maximal prefix consisting of full and valid code points was converted (and written to output).
+    // Input data is INCORRECT.
+    // Conversion is done partially, but cannot be finished due to invalid input.
+    // Maximal prefix consisting of full and valid code points was converted (and written to output).
 
     csInputOutputNoAccess = 4,
-    //Cannot work with given input/output.
-    //In case of files: cannot find or open input file, or cannot write to output file.
-    //In case of buffers: some non-empty buffer has zero pointer, or some buffer has negative size.
+    // Cannot work with given input/output.
+    // In case of files: cannot find or open input file, or cannot write to output file.
+    // In case of buffers: some non-empty buffer has zero pointer, or some buffer has negative size.
 };
 
 struct ConversionResult {
@@ -41,19 +41,19 @@ struct ConversionResult {
 
 //======================================= In-memory conversion ====================================
 
-//Convert data from one contiguous buffer in memory into another contiguous buffer in memory.
-//The buffers must not overlap, both buffer sizes are given in bytes.
-//Output buffer must be sufficiently large, otherwise csOverflowPossible error would be returned.
-//You can determine a suitable size of output buffer by calling ConvertInMemorySize function (see below).
-//In order to get a processor object, use ProcessorSelector from the same-named header file.
+// Convert data from one contiguous buffer in memory into another contiguous buffer in memory.
+// The buffers must not overlap, both buffer sizes are given in bytes.
+// Output buffer must be sufficiently large, otherwise csOverflowPossible error would be returned.
+// You can determine a suitable size of output buffer by calling ConvertInMemorySize function (see below).
+// In order to get a processor object, use ProcessorSelector from the same-named header file.
 ConversionResult ConvertInMemory(BaseBufferProcessor &processor, const char *inputBuffer, long long inputSize, char *outputBuffer, long long outputSize);
 
-//Request information about the required size of the output buffer in ConvertInMemory routine.
-//Returns the minimal allowed size of the output buffer (in bytes).
-//It is surely enough to contain conversion output for any possible input buffer of specified size.
-//Additionally, if outputBuffer points to char* with null value, then:
-//   1. a buffer of that size will be allocated (with new char[]);
-//   2. pointer to this new buffer will be saved into *outputBuffer;
+// Request information about the required size of the output buffer in ConvertInMemory routine.
+// Returns the minimal allowed size of the output buffer (in bytes).
+// It is surely enough to contain conversion output for any possible input buffer of specified size.
+// Additionally, if outputBuffer points to char* with null value, then:
+//    1. a buffer of that size will be allocated (with new char[]);
+//    2. pointer to this new buffer will be saved into *outputBuffer;
 long long ConvertInMemorySize(BaseBufferProcessor &processor, long long inputSize, char **outputBuffer = 0);
 
 
@@ -61,8 +61,8 @@ long long ConvertInMemorySize(BaseBufferProcessor &processor, long long inputSiz
 
 enum FileIOType {
     ftLibC = 0,
-    //Use fopen + fread + fwrite + fclose.
-    //Cross-platform.
+    // Use fopen + fread + fwrite + fclose.
+    // Cross-platform.
 
 
     //TODO: perhaps implement some other types?
@@ -83,7 +83,7 @@ struct ConvertFilesSettings {
     ConvertFilesSettings() : type(ftLibC)/*, bufferSize(0)*/ {}
 };
 
-//Convert data from one file to another file.
-//Both files are defined by their paths, which are passed to corresponding I/O API.
-//In order to get a processor object, use ProcessorSelector from the same-named header file.
+// Convert data from one file to another file.
+// Both files are defined by their paths, which are passed to corresponding I/O API.
+// In order to get a processor object, use ProcessorSelector from the same-named header file.
 ConversionResult ConvertFile(BaseBufferProcessor &processor, const char *inputFilePath, const char *outputFilePath, ConvertFilesSettings settings = ConvertFilesSettings());
